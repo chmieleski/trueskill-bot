981644d618f5332e31272c1bd835e3b85712126c
Augusto Silveira <augustocezarchmieleski@gmail.com>
2026-08-24 06:52:53 +0200

    feat(docs): sync Discord guides to a channel

--- src/services/docs/index.ts
+++ src/services/docs/index.ts
@@ -1,3 +1,4 @@
 export { DocsServiceError } from './docs-errors.js';
 export { loadDiscordDocs, type DiscordDocPost, type DiscordDocsKind } from './load-discord-docs.js';
+export { syncDiscordDocsToChannel, type SyncDiscordDocsResult } from './sync-discord-docs.js';
 export { wipeChannelMessages } from './wipe-channel-messages.js';
